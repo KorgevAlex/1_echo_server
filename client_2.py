23e6577b9fc39e6fc03f1fd8c9b32097311b6821
KorgevAlex <100684374+KorgevAlex@users.noreply.github.com>
2022-10-11 14:33:25 +0300

    Update client_2.py

--- client_2.py
+++ client_2.py
@@ -1,14 +1,27 @@
 import socket
 import getpass
-def port_address_name():
+def port_addr_name():
     addr = input("Input addres: ")
-    port = int(input("Input port : "))
+    port = int(input("Input port number: "))
     return (addr, port) 
+
+def inp_pass():
+    name = input('Input name: ')
+    #password =  getpass.getpass(prompt="Input password: ")
+    password = input('Input password: ')
+    return (name, password)
+
+def send_pass(sock):
+    name = input('Input name: ')
+    sock.send(name.encode())
+    password =  getpass.getpass(prompt="Input password: ")
+    #password = input('Input password: ')
+    sock.send(password.encode())
 
 def connect_to_server():
     sock = socket.socket()
-    sock.connect(port_address_name())
-    #msg = input()
+    sock.connect(port_addr_name())
+    send_pass(sock)
     msg = ""
     while True:
         msg = input()
